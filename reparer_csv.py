71c4638e440e10d9e2dbfc30a0ecc2504cd9b300
Leandre <syleandresome07@gmail.com>
2026-08-16 21:39:30 +0000

    Gestion des doublons dans reparer_csv

--- reparer_csv.py
+++ reparer_csv.py
@@ -9,3 +9,10 @@ print(lignes[0])  # {'nom': 'Awa', 'age': '19', 'filiere': 'MPSI'}
 for ligne in lignes:
     if ligne["age"] == "":
         print(f"Ligne incomplète : {ligne['nom']} n'a pas d'âge renseigné")
+
+vues = set()
+for ligne in lignes:
+    identifiant = (ligne["nom"], ligne["age"], ligne["filiere"])
+    if identifiant in vues:
+        print(f"Doublon détecté : {ligne['nom']}")
+    vues.add(identifiant)
