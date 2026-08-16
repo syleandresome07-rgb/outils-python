cc716f7c24a72cd7f32b09d2d77ac0fecc0bd8d3
Leandre <syleandresome07@gmail.com>
2026-08-16 21:38:12 +0000

    Ajoute les valeurs manquantes dans repare_csv

--- reparer_csv.py
+++ reparer_csv.py
@@ -5,3 +5,7 @@ with open("etudiants.csv", newline="") as fichier:
     lignes = list(lecteur)
 
 print(lignes[0])  # {'nom': 'Awa', 'age': '19', 'filiere': 'MPSI'}
+
+for ligne in lignes:
+    if ligne["age"] == "":
+        print(f"Ligne incomplète : {ligne['nom']} n'a pas d'âge renseigné")
